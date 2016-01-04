c9c5649e277f29e01fc2fde7797d0528e3b5d76f
motti770 <mottigottlieb@gmail.com>
2016-01-04 17:56:42 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,43 @@
+
+def using_push(countries_in_western_africa,next_country)
+      countries_in_western_africa.push(next_country)
+end
+def using_unshift(countries_in_western_africa,next_country)
+      countries_in_western_africa.unshift(next_country)
+  end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+def using_shift(arr)
+  arr.shift
+end
+def using_sort(arr)
+    arr.sort
+end
+def shift_with_args(array)
+  array.shift(2)
+end
+def using_concat(arr_one,arr_two)
+  arr_one.concat(arr_two)
+end
+def using_insert(arr, element)
+  arr.insert(4, element)
+end
+def using_uniq(arr)
+  arr.uniq
+end
+def using_flatten(private_colleges_in_newyork)
+  private_colleges_in_newyork.flatten
+end
+def using_delete(x,y)
+  x.delete(y)
+end
+def using_delete_at(famous_robots, int)
+  famous_robots.delete_at(int)
+end
